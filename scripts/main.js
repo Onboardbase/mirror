e6f9a70ca32d8545dfdca62ac6aae59b4adff478
Dante Lex <dantelex2@gmail.com>
2023-08-29 14:43:09 +0100

    Styled modal

--- scripts/main.js
+++ scripts/main.js
@@ -159,7 +159,7 @@ function throttle(func, limit) {
 function injectTailwindCSS() {
     const linkElement = document.createElement("link");
     linkElement.rel = "stylesheet";
-    linkElement.href = "https://cdn.tailwindcss.com/2.2.16/tailwind.min.css";
+    linkElement.href = "https://cdn.tailwindcss.com/3.3.3/tailwind.min.css";
     document.head.appendChild(linkElement);
 }
   
@@ -169,30 +169,33 @@ function createTailwindModal(keys) {
   const modalHTML = `
     <div id="keyModal" class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+        <div class="fixed inset-0 bg-gray-900 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-            <div class="sm:flex sm:items-start">
-              <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                  Detected keys
-                </h3>
-                <div class="mt-2">
-                  <p class="text-sm text-gray-500">
-                    ${keys.join('<br>')}
-                  </p>
-                </div>
-              </div>
-            </div>
+        <div class="inline-block align-bottom bg-white rounded-lg text-left shadow-xl relative transform transition-all sm:my-8 sm:align-middle sm:max-w-xl sm:w-full">
+          <div class="flex items-center py-3.5 px-4 border-b border-gray-100/5">
+            <h2 class="text-xl leading-4 font-extrabold text-black">Detected keys</h2>
+            <p class="text-sm leading-5 font-medium text-gray-500">add to Onboardbase</p>
           </div>
-          <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-            <button id="addToOnboardbase" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm">
-              Add to Onboardbase
-            </button>
-            <button id="modalCancel" type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+          <div class="p-3 bg-cool-gray-50 border-b border-gray-100/5 h-60 overflow-hidden text-sm text-gray-500">
+            ${keys.join('<br>')}
+          </div>
+          <div class="px-4 py-3 sm:flex items-center">
+            <button id="modalCancel" type="button" class="w-full inline-flex justify-center rounded-md border border-gray-200 px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:w-auto sm:text-sm">
               Cancel
             </button>
+            <button id="addToOnboardbase" type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-500 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none sm:ml-3 sm:w-auto sm:text-sm">
+              Add
+            </button>
+          </div>
+          <div
+            class="flex flex-row rot items-center justify-start origin-bottom-left border border-gray-100/10 border-b-0 py-1.5 px-3 gap-1 rounded-b-none absolute left-0 -ml-[1px] top-0 rounded-md bg-black"
+          >
+            <span class="text-so-small font-semibold text-gray-200">Secured by</span>
+            <img
+              class="mx-auto h-2.5 w-auto"
+              src="../img/onboardbase-2.png"
+              alt="Onboardbase logo"
+            />
           </div>
         </div>
       </div>
